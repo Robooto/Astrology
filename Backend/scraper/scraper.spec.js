62615e69f1a25e5995dca01d2042f3c38a30b080
Sam Russell <samrussell63@gmail.com>
2018-01-15 20:55:49 -0800

    adding some new tests and fixing the old ones

--- Backend/scraper/scraper.spec.js
+++ Backend/scraper/scraper.spec.js
@@ -5,9 +5,10 @@ const chaiAsPromised = require("chai-as-promised");
 const cheerio = require('cheerio');
 const request = require('request');
 const staticData = require('../static/horoscopeData');
- 
+
 chai.use(chaiAsPromised);
 const expect = chai.expect;
+const should = chai.should();
 
 describe('Scraper', () => {
 
@@ -32,26 +33,50 @@ describe('Scraper', () => {
         });
     });
 
-    describe('getWebsiteHtml', function() {
-        after(function () {
+    describe('getWebsiteHtml', function () {
+        afterEach(function () {
             request.get.restore();
         });
         it('should reject if promise fails', () => {
-            sinon.stub(request, 'get').returns('error', null, null);
+            sinon.stub(request, 'get').yields('error', null, null);
             const url = `https://www.freewillastrology.com/horoscopes/allsigns.html`;
-            expect(scraper.getWebsiteHtml(url)).to.be.rejected;
+            return expect(scraper.getWebsiteHtml(url)).to.be.rejected;
+        });
+
+        it('should fufil if the request returns anything', () => {
+            sinon.stub(request, 'get').yields(null, null, `<body></body>`);
+            const url = `https://www.freewillastrology.com/horoscopes/allsigns.html`;
+            return expect(scraper.getWebsiteHtml(url)).to.be.fulfilled;
+        })
+    });
+
+    describe('parseHoroscopeData', function () {
+        it('should reject if there is no dom elements', () => {
+            return expect(scraper.parseHoroscopeData('<html></html>')).to.be.rejected;
+        });
+
+        it('should return parsed data', () => {
+            return expect(scraper.parseHoroscopeData(`<body><div><table></table><table><tbody><tr><td></td><td><div>test*pastcl\nasdf</div></td></tr><tr><td></td><td><div>test*pastcl\nasdf</div></td></tr></tbody></table></div></body>`)).to.be.fulfilled;
         });
     });
 
-    describe('parseHoroscopeData', function() {
-        it('should reject if there is no dom elements', () => {         
-            expect(scraper.parseHoroscopeData('<html></html>')).to.be.rejected;
-        });
-    });
-
-    describe('addHoroscopes', function() {
+    describe('addHoroscopes', function () {
         it('should reject if there is no parsed info', () => {
-            expect(scraper.addHoroscopes(null, staticData)).to.be.rejected;
+            return expect(scraper.addHoroscopes(null, staticData)).to.be.rejected;
+        });
+
+        it('should fulfill if there is parsed info', () => {
+            return expect(scraper.addHoroscopes({
+                textDate: 'Horoscopes for week of January 11, 2018',
+                date: '2018-01-11T08:00:00.000Z',
+                horoscopes: [
+                    {
+                        "name": "Aries",
+                        "dateRange": "March 21 - April 19",
+                        "lowDate": "2018-03-21T07:00:00.000Z",
+                        "highDate": "2018-04-19T07:00:00.000Z"
+                    }]
+            }, staticData)).to.eventually.have.property("date");
         });
     });
 });
