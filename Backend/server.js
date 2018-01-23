b90b7a2e50eca4c2b67bed14d33b661b313f9466
Sam Russell <samrussell63@gmail.com>
2018-01-22 20:47:50 -0800

    adding in the frontend ng template

--- Backend/server.js
+++ Backend/server.js
@@ -9,6 +9,7 @@ const scopesController = require('./controllers/scopes.controller');
 app.use(cors());
 app.use(bodyParser.json());
 
+// add routes
 app.use('/scopes', scopesController);
 
 app.listen(3000, () => console.log(`app running on port 3000`));
